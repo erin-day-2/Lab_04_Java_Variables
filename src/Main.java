a7e98f4aba0d2d515b989aed464200390589aebb
erin-day-2 <daye4@mail.uc.edu>
2022-02-04 14:18:35 -0500

    Lab 04 Work

---
+++ src/Main.java
@@ -0,0 +1,80 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	int OperandA = 24;
+    int OperandB = 12;
+    int sum = 13;
+    int product = 27;
+    int difference = 25;
+    int quotient = 36;
+    int modulo = 37;
+
+    double doubleOperandA = 1.1;
+    double doubleOperandB = 2.2;
+    double doubleSum = 2.4;
+    double doubleProduct = 3.6;
+    double doubleDifference = 5.8;
+    double doubleQuotient = 7.9;
+
+    sum = OperandA + OperandB;
+
+    System.out.println("The sum of " + OperandA + " and " + OperandB + " is " + sum);
+
+    product = OperandA * OperandB;
+
+    System.out.println("The product of " + OperandA + " and " + OperandB + " is " + product);
+
+    difference =  OperandA - OperandB;
+
+    System.out.println("The difference of " + OperandA + " and " + OperandB + " is " + difference);
+
+    quotient = OperandA / OperandB;
+
+    System.out.println("The quotient of " + OperandA + " and " + OperandB + " is " + quotient);
+
+    modulo = OperandA % OperandB;
+
+    System.out.println("The modulo of " + OperandA + " and " + OperandB + " is " + modulo);
+
+
+    doubleSum = doubleOperandA + doubleOperandB;
+
+    System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+    doubleProduct = doubleOperandA * doubleOperandB;
+
+    System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+    doubleDifference = doubleOperandA - doubleOperandB;
+
+    System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+    doubleQuotient = doubleOperandA + doubleOperandB;
+
+    System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+    int numberOfKids = 4;
+    String raining = "Yes";
+    double gasPrice = 3.06;
+    int favNum = 26;
+    double shoeSize = 8.5;
+    int birthMonth = 4;
+    String fullName = "Erin Day";
+
+    System.out.println("The number if kids in my family is: " + numberOfKids);
+
+    System.out.println("Is it raining today? " + raining);
+
+    System.out.println("The price of a gallon of gas is: " + gasPrice);
+
+    System.out.println("My favorite number is: " + favNum);
+
+    System.out.println("My shoe size is: " + shoeSize);
+
+    System.out.println("My birth month is: " + birthMonth);
+
+    System.out.println("My full name is: " + fullName);
+    }
+}
